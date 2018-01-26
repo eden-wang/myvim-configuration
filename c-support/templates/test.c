59605e45f8e34c37d1dfe06d33fde2ba29b46fee
pengquan wang <pengquan.wang@dasudian.com>
2018-01-26 09:02:46 +0800

    init

---
+++ c-support/templates/test.c
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  12/30/2016 02:17:47 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  eden(pengquan.wang@dasudian.com) (), 
+ *   Organization:  www.dasudian.com
+ *
+ * =====================================================================================
+ */
+
+
